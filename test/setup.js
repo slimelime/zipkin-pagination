98cb31a0087644996b976ccf1cee109660b47491
Louis Suo <louis.suo@myob.com>
2018-08-22 16:02:51 +1000

    add ut cases

--- test/setup.js
+++ test/setup.js
@@ -1,2 +1,9 @@
+'use strict';
 require("babel-core/register");
 require("babel-polyfill");
+const chai = require('chai');
+chai.should();
+
+chai.use(require('chai-as-promised'));
+chai.use(require('sinon-chai'));
+chai.use(require('chai-shallow-deep-equal'));
--- test/zipkinFetchSpec.js
+++ test/zipkinFetchSpec.js
@@ -1,17 +1,143 @@
 const r = require('ramda');
-const assert = require('assert');
+const sandbox = require('sinon').createSandbox();
+const axios = require('axios');
+
 const {zipkinFetch} = require('../src/zipkin-fetch');
 
+const TEST_PAGINATION = 2
+const defaultDuration = 100;
+
 describe('zipkinFetch tests', function() {
-
+  afterEach(() => {
+    sandbox.restore();
+  });
   it('should return metrics within default limit 5000', async () => {
-
-    assert.equal(1, 1)
-
+    const data = [
+      [
+        {
+          timestamp: 1,
+          duration: defaultDuration,
+          tags: defaultTags,
+        }, {parentId: 1}]];
+    sandbox.stub(axios, 'get').resolves({data});
+    const res = await zipkinFetch('url', [], 'serviceName')(1, 2)
+    res.should.be.eql(data)
   })
 
-  it('should return metrics breaks the limit 5000 in given time period', async () => {
-    assert.equal(2, 2)
-  })
+  it('should return metrics exceed the limit 5000 in given time period',
+    async () => {
+      const page1 = [
+        [
+          {
+            traceId: 1,
+            timestamp: 7770000,
+            duration: defaultDuration,
+            tags: {
+              'http.host': 'api.banking-rules.prod.myob.com',
+              'http.method': 'GET',
+              'http.status_code': '200',
+              'http.status_line': '200 OK',
+              'http.url': 'https://api.banking-rules.prod.myob.com/health-check',
+            },
+          }, {parentId: 1}],
+        [
+          {
+            traceId: 2,
+            timestamp: 7770000,
+            duration: defaultDuration,
+            tags: {
+              'http.host': 'api.banking-rules.prod.myob.com',
+              'http.method': 'GET',
+              'http.status_code': '200',
+              'http.status_line': '200 OK',
+              'http.url': 'https://api.banking-rules.prod.myob.com/health-check',
+            },
+          }, {parentId: 2}]];
+      const page2 = [
+        [
+          {
+            traceId: 3,
+            timestamp: 6660000,
+            duration: defaultDuration,
+            tags: {
+              'http.host': 'api.banking-rules.prod.myob.com',
+              'http.method': 'GET',
+              'http.status_code': '200',
+              'http.status_line': '200 OK',
+              'http.url': 'https://api.banking-rules.prod.myob.com/health-check',
+            },
+          }, {parentId: 3}]];
+      sandbox.stub(axios, 'get').
+        onFirstCall().
+        returns({data: page1}).
+        onSecondCall().
+        returns({data: page2});
+      const res = await zipkinFetch('url', [], 'serviceName', TEST_PAGINATION)(
+        1, 10)
+      res.should.be.eql(r.concat(page1, page2))
+
+    })
+
+  it('should remove duplicated traceId cross response',
+    async () => {
+      const page1 = [
+        [
+          {
+            traceId: 1,
+            timestamp: 7770000,
+            duration: defaultDuration,
+            tags: defaultTags,
+          }, {parentId: 1}],
+        [
+          {
+            traceId: 2,
+            timestamp: 7770000,
+            duration: defaultDuration,
+            tags: defaultTags,
+          }, {parentId: 2}]];
+      const page2 = [
+        [
+          {
+            traceId: 1,
+            timestamp: 6660000,
+            duration: defaultDuration,
+            tags: defaultTags,
+          }, {parentId: 3}]];
+      sandbox.stub(axios, 'get').
+        onFirstCall().
+        returns({data: page1}).
+        onSecondCall().
+        returns({data: page2});
+      const res = await zipkinFetch('url', [], 'serviceName', TEST_PAGINATION)(
+        1, 10)
+      res.should.be.eql(page1)
+    })
+
+  it('should not fetch more if not the exact page size limit returned',
+    async () => {
+      const page1 = [
+        [
+          {
+            traceId: 1,
+            timestamp: 7770000,
+            duration: defaultDuration,
+            tags: defaultTags,
+          }, {parentId: 1}]];
+      sandbox.stub(axios, 'get').
+        onFirstCall().
+        returns({data: page1}).
+        onSecondCall().throws('error', 'unexpected fetching');
+      const res = await zipkinFetch('url', [], 'serviceName', TEST_PAGINATION)(
+        1, 10)
+      res.should.be.eql(page1)
+    })
 
 });
+
+const defaultTags = {
+  'http.host': 'host',
+  'http.method': 'GET',
+  'http.status_code': '200',
+  'http.status_line': '200 OK',
+  'http.url': 'https://host/health-check',
+};
