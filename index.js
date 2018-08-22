421597b46acda6b9d110221e75bbcf6529534599
Louis Suo <louis.suo@myob.com>
2018-08-22 11:23:58 +1000

    export function zipkinFetch

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
-const zipkinFetch = require('./src/zipkin-fetch')
-exports.fetch = (ZIPKIN_URL, filters = []) => async (start, end) => zipkinFetch(
-  ZIPKIN_URL, filters)(start, end)
+const {zipkinFetch} = require('./src/zipkin-fetch')
+exports.zipkinFetch = zipkinFetch
